77d74137d0c4df4391537de2f2df7d0d3cba9dfd
Sebastian Ortiz Velez <sebastia.ortiz@starshotsoftware.com>
2018-02-24 21:57:29 +0100

    added language and translations model

---
+++ Lands/Lands/Models/Language.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Lands.Models
+{
+    public class Language
+    {
+        [JsonProperty(PropertyName = "iso6391")]
+        public string Iso6391 { get; set; }
+
+        [JsonProperty(PropertyName = "iso6392")]
+        public string Iso6392 { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "nativename")]
+        public string NativeName { get; set; }
+    }
+}
