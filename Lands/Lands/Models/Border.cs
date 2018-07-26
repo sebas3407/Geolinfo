6799a7a317f09dd51138794f2ecb663385833dae
Sebastian Ortiz Velez <sebastia.ortiz@starshotsoftware.com>
2018-07-26 13:19:49 +0200

    Borders page

--- Lands/Lands/Models/Border.cs
+++ Lands/Lands/Models/Border.cs
@@ -3,11 +3,9 @@ namespace Lands.Models
 {
     public class Border
     {
-		private string alpha3Code;
-
 		public Border(string alpha3Code, string name)
 		{
-			this.alpha3Code = alpha3Code;
+			Code = alpha3Code;
 			Name = name;
 		}
 
