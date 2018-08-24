9dc4a334914f185b2633edbd67c73c2511077759
Sebastian Ortiz Velez <sebasortiz2000@gmail.com>
2018-08-24 19:40:29 +0200

    Working on multi language system

---
+++ Lands/Lands/Interfaces/Languages.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Lands.Interfaces;
+
+public static class Languages
+{
+    static Languages()
+    {
+        var ci = DependencyService.Get<Ilocalize>().GetCurrentCultureInfo();
+       
+        //SEBAS
+        //Resource.Culture = ci;
+        //DependencyService.Get<ILocalize>().SetLocale(ci);
+    }
+}
